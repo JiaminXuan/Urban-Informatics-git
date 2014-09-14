df498b7d22a82effa9e50cabac5be26b598e44f7
JiaminXuan <JiaminXuan@users.noreply.github.com>
2014-09-14 01:52:28 -0400

    Create problem6.py

---
+++ problem6.py
@@ -0,0 +1,17 @@
+import sys
+from pandas import DataFrame as df
+zip_code=[]
+count_zip=[]
+rawdata=df.from_csv(sys.argv[1]).sort('Incident Zip')
+Department=rawdata['Agency']
+for item in sorted(set(Department)):
+	print item,
+	for i in range(len(Department)):
+		if Department.iloc[i]==item:
+			print str(rawdata.iloc[i,6]),
+			zip_code.append(rawdata.iloc[i,6])
+	for unique_zip in set(zip_code):
+		countnum=0
+		if zip_code.count(unique_zip)>countnum:
+			countnum=zip_code.count(unique_zip)
+	print countnum
